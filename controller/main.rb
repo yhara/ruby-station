e82ef82ea04c89126b3c8a4224dbc992a1484523
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-02-11 11:04:56 +0900

    new: install and exec

--- controller/main.rb
+++ controller/main.rb
@@ -6,15 +6,74 @@
 # this will force the controller to be mounted on: /otherurl
 
 class MainController < Controller
-  # the index action is called automatically when no other action is specified
+
   def index
-    @title = "Welcome to Ramaze!"
+    # TEMP: shutdown all apps
+#    Application.keys.each{|k| 
+#      app = Application[k]
+#      app[:pid] = nil
+#      Application[k] = app
+#    }
+    #Application.clear
+
+    @applications = Application.original
+    # index.mab
   end
 
-  # the string returned at the end of the function is used as the html body
-  # if there is no template for the action. if there is a template, the string
-  # is silently ignored
-  def notemplate
-    "there is no 'notemplate.xhtml' associated with this action"
+  def add
+    tempfile = request["gem"][:tempfile]
+    @filename = request["gem"][:filename]
+    @size = tempfile.size
+    session[:tempfile] = tempfile
+    session[:filename] = @filename
   end
+
+  def do_add
+    filename = session[:filename]
+    newpath = "/tmp/a.gem"
+    File.symlink(session[:tempfile].path, newpath)
+    @result = h `gem1.9 install #{newpath}`
+    File.unlink(newpath)
+
+    Application.clear
+    id = Application.size
+    Application[id] = {
+      :id => id,
+      :name => filename,
+      :port => 30000 + rand(9999),
+      :pid => nil
+    }
+  end
+  deny_layout :do_add
+
+  def start(id)
+    id = id.to_i
+    app = Application.original[id]
+    raise "id #{id} not found" unless app
+
+    start_cmd = "ruby1.9 #{gem_home}/#{File.basename(app[:name], ".gem")}/start.rb -p #{app[:port]}"
+
+    Ramaze::Log << "!!!!!!!! #{start_cmd}"
+    fork {
+      pid = exec(start_cmd)
+    }
+#    pid = Process.spawn(start_path)
+#    Ramaze::Log << "spawned!! #{pid}"
+    pid = ":-("
+
+    app[:pid] = pid
+    Application[id] = app
+    sleep 1
+    redirect "http://localhost:#{app[:port]}/"
+  end
+
+  def stop(id)
+    app[:pid] = nil
+    redirect :index
+  end
+
+  def gem_home
+    "/Users/yhara/gems1.9"
+  end
+
 end
