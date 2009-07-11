e3b182fa69b394ce5120df4252cb13d393f707e4
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-07-11 23:17:44 +0900

    new: App#start

--- model/application.rb
+++ model/application.rb
@@ -6,5 +6,41 @@ class Application
   property :port, Integer
   property :name, String
   property :version, String
+
+  def start
+    return if self.pid
+
+    cmd = [
+      Conf.ruby_command,
+      script_path,
+      "--port", self.port.to_s,
+      "--data-dir", data_dir,
+    ].join(" ")
+
+    self.pid = fork {
+      exec(cmd)
+    }
+    self.save
+  end
+
+  def stop
+    return unless self.pid
+    raise NotImplementedError
+  end
+
+  private
+
+  def script_path
+    File.join(Conf.gem_dir, "gems",
+              full_name, "main.rb")
+  end
+
+  def data_dir
+    File.join(Conf.data_dir, full_name)
+  end
+
+  def full_name
+    "#{self.name}-#{self.version}"
+  end
 end
 DataMapper.auto_upgrade!
