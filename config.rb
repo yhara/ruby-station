ab58615240aed00fbe1336e394ff191556e44ee1
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-07-17 18:19:45 +0900

    Install appkit on start

--- config.rb
+++ config.rb
@@ -3,7 +3,17 @@ require 'fileutils'
 
 #Encoding.default_external = 'UTF-8'
 
+module RubyStation
+  VERSION = File.read(File.expand_path("./VERSION", File.dirname(__FILE__))).chomp
+end
+
 class Conf
+  %w(ruby_command gem_command gem_dir gem_bin_dir gem_install_option data_dir).each do |m|
+    module_eval <<-EOD
+      def self.#{m}; @yaml[:#{m}]; end
+    EOD
+  end
+
   @home = File.expand_path("~/.ruby-station")
   @yaml = YAML.load_file(File.join(@home, "config.yaml"))
   @yaml[:gem_dir] = File.expand_path(@yaml[:gem_dir], @home)
@@ -13,9 +23,13 @@ class Conf
   FileUtils.makedirs(@yaml[:gem_bin_dir])
   FileUtils.makedirs(@yaml[:data_dir])
 
-  %w(ruby_command gem_command gem_dir gem_bin_dir gem_install_option data_dir).each do |m|
-    module_eval <<-EOD
-      def self.#{m}; @yaml[:#{m}]; end
-    EOD
+  def self.installed?(fullname)
+    File.exist?(File.join(self.gem_dir, fullname))
   end
+
+  def self.install_appkit
+    GemManager.install_file(__DIR__("pkg/ruby-station-#{RubyStation::VERSION}.gem"))
+  end
+
+  install_appkit unless installed?("ruby-station-#{RubyStation::VERSION}")
 end
