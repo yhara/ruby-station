1839f5f2a7143a77877378c1fe4353f65be34294
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-07-17 17:25:49 +0900

    warn if port/data-dir is not set

--- lib/ruby-station.rb
+++ lib/ruby-station.rb
@@ -9,6 +9,15 @@ module RubyStation
       o.on("--port N"){|n| @port = n}
       o.on("--data-dir PATH"){|d| @data_dir = d}
     }.parse!(ARGV)
+
+    unless @port
+      @port = 40000 + rand(10000)
+      warn "--port is not specified; assuming it is #{@port}"
+    end
+    unless @data_dir
+      @data_dir = File.expand_path("./")
+      warn "--data-dir is not specified; assuming it is #{@data_dir}" 
+    end
   end
 
   def self.port; @port; end
