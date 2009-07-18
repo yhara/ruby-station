639864234b7e023b1be7ee0942da76675f663435
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-07-19 00:23:11 +0900

    Moved to bin/; copy config.yaml if not exist

--- config.rb
+++ config.rb
@@ -8,23 +8,52 @@ module RubyStation
 end
 
 class Conf
-  %w(ruby_command gem_command gem_dir gem_bin_dir gem_install_option data_dir).each do |m|
+  %w(ruby_command gem_command gem_dir gem_bin_dir gem_install_option data_dir server_port).each do |m|
     module_eval <<-EOD
       def self.#{m}; @yaml[:#{m}]; end
     EOD
   end
 
-  @home = File.expand_path("~/.ruby-station")
-  @yaml = YAML.load_file(File.join(@home, "config.yaml"))
-  @yaml[:gem_dir] = File.expand_path(@yaml[:gem_dir], @home)
-  @yaml[:gem_bin_dir] = File.expand_path(@yaml[:gem_bin_dir], @home)
-  @yaml[:data_dir] = File.expand_path(@yaml[:data_dir], @home)
-  FileUtils.makedirs(@yaml[:gem_dir])
-  FileUtils.makedirs(@yaml[:gem_bin_dir])
-  FileUtils.makedirs(@yaml[:data_dir])
+  def self.parse_opt
+    @home = File.expand_path("~/.ruby-station")
 
-  unless GemManager.installed?("ruby-station", RubyStation::VERSION)
-    gem_path = __DIR__("pkg/ruby-station-#{RubyStation::VERSION}.gem")
-    GemManager.install_file(gem_path)
+    @opt = OptionParser.new{|o|
+      o.on("--home PATH", "path to save ruby-station data (default: #@home)"){|path|
+        @home = path
+      }
+      o.on("-h", "--help", "show this message"){
+        puts @opt.to_s
+        exit
+      }
+    }
+    @opt.parse!(ARGV)
+  end
+
+  def self.load_yaml
+    yaml_path = File.join(@home, "config.yaml")
+    unless File.exist?(yaml_path)
+      Ramaze::Log.warn "#{yaml_path} not found: creating"
+      FileUtils.makedirs(@home)
+      File.open(yaml_path, "w"){|f|
+        f.write File.read(__DIR__("sample.config.yaml"))
+      }
+    end
+    YAML.load_file(yaml_path)
+  end
+
+  # TODO: refactor X-(
+  def self.init
+    @yaml = self.load_yaml
+    @yaml[:gem_dir] = File.expand_path(@yaml[:gem_dir], @home)
+    @yaml[:gem_bin_dir] = File.expand_path(@yaml[:gem_bin_dir], @home)
+    @yaml[:data_dir] = File.expand_path(@yaml[:data_dir], @home)
+    FileUtils.makedirs(@yaml[:gem_dir])
+    FileUtils.makedirs(@yaml[:gem_bin_dir])
+    FileUtils.makedirs(@yaml[:data_dir])
+
+    unless GemManager.installed?("ruby-station", RubyStation::VERSION)
+      gem_path = __DIR__("pkg/ruby-station-#{RubyStation::VERSION}.gem")
+      GemManager.install_file(gem_path)
+    end
   end
 end
