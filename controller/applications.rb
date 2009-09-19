25c5f67508b64c344b91d793366787b1f4f909bc
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-09-19 22:27:33 +0900

    Applications#foo (just a test)

--- controller/applications.rb
+++ controller/applications.rb
@@ -1,8 +1,13 @@
+require 'json'
+
 class Applications < Controller
   map '/applications'
+  provide :json, :type => "application/json" do |action, value|
+    value.to_json
+  end
 
   layout{|path, ext|
-    "default" unless path =~ /\Ado_/
+    "default" unless path =~ /\A_/
   }
 
   def install
@@ -17,9 +22,13 @@ class Applications < Controller
     end
   end
 
-  def do_install
+  def _install
+    session[:messages] = []
     if gemname = session[:gemname]
-      result, name, version = GemManager.install_gem(gemname)
+      result, name, version = GemManager.install_gem(gemname){|line|
+        session[:messages] << line
+      }
+      session[:messages] << :__end__
     elsif session[:tempfile]
       path = session[:tempfile].path
       result, name, version = GemManager.install_file(path)
@@ -27,7 +36,7 @@ class Applications < Controller
       raise
     end
 
-    unless Application.first(:name => name, :version => version)
+    if result and not Application.first(:name => name, :version => version)
       Application.create({
         :pid => nil,
         :port => 30000 + rand(9999),
@@ -38,6 +47,22 @@ class Applications < Controller
 
     session.clear
     h result
+  end
+
+  def _message
+    return "['error']" if session[:messages].nil?
+
+    sleep 1 while session[:messages].empty?
+
+    is_end = "false"
+    if session[:messages].last == :__end__
+      session[:messages].pop
+      is_end = "true"
+    end
+
+    mes = session[:messages].map{|line| "#{h line.chomp}<br>"}.join
+    session[:messages].clear
+    "[#{is_end}, \"#{mes}\"]"
   end
 
   def create
@@ -79,6 +104,34 @@ class Applications < Controller
     app.stop
 
     redirect_referer
+  end
+
+  def foo
+    session[:foo] = []
+  end
+
+  def _foo_gen
+    5.times do |i|
+      session[:foo].push "this is line #{i}"
+      sleep 1
+    end
+    session[:foo].push :__end__
+  end
+
+  def _foo_get
+    if session[:foo].empty?
+      [nil]
+    else
+      if session[:foo].last == :__end__
+        is_end = true
+        session[:foo].pop
+      else
+        is_end = false
+      end
+      mes = session[:foo].map{|line| (h line.chomp)+"<br>\n"}.join
+      session[:foo].clear
+      [is_end, mes]
+    end
   end
 
 end
