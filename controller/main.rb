7abd3b7cca8cd02be1f645795c3c3d7d822f211e
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-09-22 23:37:33 +0900

    new: /notfound

--- controller/main.rb
+++ controller/main.rb
@@ -1,8 +1,14 @@
 class MainController < Controller
   map '/'
+  helper :flash
 
   def index
     @applications = Application.all
+  end
+
+  def notfound
+    @message = flash[:error] || "The page you requested is not found."
+    response.status = 404
   end
 
 end
