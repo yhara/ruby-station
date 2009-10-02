8e5d7ad9279017cc86d32638fa77ff2e14ffa020
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-10-02 23:17:47 +0900

    Replaced test gems

--- tests/spec/gem_manager.rb
+++ tests/spec/gem_manager.rb
@@ -5,23 +5,25 @@ require TESTS_DIR/"../util/gem_manager.rb"
 
 describe GemManager do
   it "should install a gem via file" do
-    path = TESTS_DIR/"data/hello/pkg/hello-0.2.0.gem"
+    path = TESTS_DIR/"data/hello/pkg/hello-ruby-station-0.3.2.gem"
     result, name, version = GemManager.install_file(path)
 
     result.should be_instance_of(String)
-    name.should == "hello"
-    version.should == "0.2.0"
+    name.should == "hello-ruby-station"
+    version.should == "0.3.2"
 
-    GemManager.installed?("hello", "0.2.0").should be_true
+    GemManager.installed?("hello-ruby-station", "0.3.2").should be_true
   end
 
   it "should install a gem via network" do
-    pending
+    GemManager.install_gem("hello-ruby-station")
+
+    GemManager.installed?("hello-ruby-station", "0.3.0").should be_true
   end
   
   it "should uninstall a gem" do
-    GemManager.uninstall("hello", "0.2.0")
+    GemManager.uninstall("hello-ruby-station", "0.3.2")
 
-    GemManager.installed?("hello", "0.2.0").should be_false
+    GemManager.installed?("hello-ruby-station", "0.3.2").should be_false
   end
 end
