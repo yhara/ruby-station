937b0d4a088df770ce77397710d1dd5f434b8be8
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-10-04 16:42:45 +0900

    fix: should read application.rb by relative path

--- model/init.rb
+++ model/init.rb
@@ -2,4 +2,4 @@ require 'dm-core'
 Ramaze::Log.info("opening database: #{Conf.db_path}")
 DataMapper.setup(:default, "sqlite3://#{Conf.db_path}")
 
-require 'model/application'
+require __DIR__("application.rb")
