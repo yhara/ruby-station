82c6f9774f44ae1e98c1e5153323d1ab8f68c418
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-10-05 22:36:30 +0900

    new: copy data files from older version

--- tests/features/step_definitions/application_steps.rb
+++ tests/features/step_definitions/application_steps.rb
@@ -7,9 +7,23 @@ def install_hello(name, version)
   Application.install(:file, hello_gem_path(version))
 end
 
-Given /^I have '(.*) (.*)'$/ do |name, version|
+def data_dir_of(name, version)
+  File.expand_path("#{name}-#{version}", Conf.data_dir)
+end
+
+def test_txt_path(name, version)
+  File.expand_path("test.txt", data_dir_of(name, version))
+end
+
+Given /^I have '(.*) (.*)'( and its data)?$/ do |name, version, data|
   unless find_app(name, version)
     install_hello(name, version)
+  end
+  if data
+    File.open(test_txt_path(name, version), "w"){|f|
+      # Note: content of test data file should be unique
+      f.puts version
+    }
   end
 end
 
@@ -33,8 +47,23 @@ Then /^I should not have '(.*) (.*)'$/ do |name, version|
   find_app(name, version).should be_nil
 end
 
-Then /data files of '(.*) (.*)' is copied to '(.*) (.*)'/ do |n1, v1, n2, v2|
-  data_path(n1, v1)
-  data_path(n2, v2)
+def files_of(dir)
+  Dir["#{dir}/**"].map{|path| path.sub(dir, "")}
 end
 
+Then /^data files of '(.*) (.*)' should exist$/ do |name, version|
+  Dir.entries(data_dir_of(name, version)).should_not be_empty
+end
+
+Then /^data files of '(.*) (.*)' is same as '(.*) (.*)'/ do |n1, v1, n2, v2|
+  files1 = files_of(data_dir_of(n1, v1)).sort
+  files2 = files_of(data_dir_of(n2, v2)).sort
+  files1.should == files2
+
+  files1.each do |rel_path|
+    path1 = File.join(data_dir_of(n1, v1), rel_path)
+    path2 = File.join(data_dir_of(n2, v2), rel_path)
+
+    File.read(path1).should == File.read(path2)
+  end
+end
--- util/gem_manager.rb
+++ util/gem_manager.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'open3'
 
 module GemManager
@@ -33,9 +34,10 @@ module GemManager
 
       out, _ = gem_install(cmd)
       spec = YAML.load(`gem spec #{path}`)
-      make_data_dir(File.join(Conf.data_dir, "#{spec.name}-#{spec.version}"))
+      name, version = spec.name, spec.version.to_s
+      make_data_dir(name, version)
 
-      [out, spec.name, spec.version.to_s]
+      [out, name, version]
     ensure
       File.unlink(newpath) if symlinked
     end
@@ -50,7 +52,7 @@ module GemManager
     ].join(" ")
     
     out, version = gem_install(cmd, name)
-    make_data_dir(File.join(Conf.data_dir, "#{name}-#{version}"))
+    make_data_dir(name, version)
 
     [out, name, version]
   end
@@ -70,11 +72,27 @@ module GemManager
     return out, $1
   end
 
-  def self.make_data_dir(data_dir)
+  def self.make_data_dir(name, version)
+    data_dir = File.join(Conf.data_dir, "#{name}-#{version}")
     unless File.directory?(data_dir)
-      Dir.mkdir(data_dir)
-      Ramaze::Log.info "made data dir for the gem: #{data_dir}"
+      if prev_ver = previous_version(name, version)
+        prev_dir = File.join(Conf.data_dir, "#{name}-#{prev_ver}")
+        FileUtils.cp_r(prev_dir, data_dir)
+        Ramaze::Log.info "copied data of #{version} for the gem: #{data_dir}"
+      else
+        Dir.mkdir(data_dir)
+        Ramaze::Log.info "made data dir for the gem: #{data_dir}"
+      end
     end
+  end
+
+  def self.previous_version(name, version)
+    specs_dir = File.join(Conf.gem_dir, "specifications")
+    index = Gem::SourceIndex.new.load_gems_in(specs_dir)
+
+    req = Gem::Requirement.create("< #{version}")
+    prev_spec = index.find_name(name, req).last
+    return prev_spec ? prev_spec.version.to_s : nil
   end
 
   def self.uninstall(name, version)
